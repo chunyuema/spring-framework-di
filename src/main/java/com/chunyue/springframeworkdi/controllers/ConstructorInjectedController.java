519ef402c4c7f44e1036f4ee19b59409b8a28a5a
Chunyue Ma <cma01@wesleyan.edu>
2021-07-03 13:56:01 -0400

    constructor based dependency injection, most favored

---
+++ src/main/java/com/chunyue/springframeworkdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,15 @@
+package com.chunyue.springframeworkdi.controllers;
+
+import com.chunyue.springframeworkdi.services.GreetingService;
+
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService){
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
