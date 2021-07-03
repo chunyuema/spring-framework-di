cb4e6e2ae55f3a65fb8c5161d89218ee9e3dd2e8
Chunyue Ma <cma01@wesleyan.edu>
2021-07-03 13:48:55 -0400

    setter based dependency injection

---
+++ src/main/java/com/chunyue/springframeworkdi/controllers/SetterInjectedController.java
@@ -0,0 +1,15 @@
+package com.chunyue.springframeworkdi.controllers;
+
+import com.chunyue.springframeworkdi.services.GreetingService;
+
+public class SetterInjectedController {
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/test/java/com/chunyue/springframeworkdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,23 @@
+package com.chunyue.springframeworkdi.controllers;
+
+import com.chunyue.springframeworkdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller= new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void sayGreeting() {
+        System.out.println(controller.sayGreeting());
+    }
+}
