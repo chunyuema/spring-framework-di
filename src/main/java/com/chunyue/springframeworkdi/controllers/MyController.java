1611d5392dafadb4e5c7ef7981579f1d46c6bfd4
Chunyue Ma <cma01@wesleyan.edu>
2021-07-04 18:01:33 -0400

    add primary bean for default service injection into controllers

--- src/main/java/com/chunyue/springframeworkdi/controllers/MyController.java
+++ src/main/java/com/chunyue/springframeworkdi/controllers/MyController.java
@@ -1,11 +1,17 @@
 package com.chunyue.springframeworkdi.controllers;
 
+import com.chunyue.springframeworkdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello World");
-        return "Hi folks";
+        return greetingService.sayGreeting();
     }
 }
